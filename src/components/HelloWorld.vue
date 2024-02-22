e8ee787f7de1abeaec018e017e904b9a5bec5ecd
Valeria Hz <valeriahz.137@gmail.com>
2024-02-21 22:49:10 -0800

    sisi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,18 +4,13 @@
 <template>
   <div>
 
-    <div class="fondo">
-        <section class="logo">
+    <div class="logo">    
             <img src="./../assets/evan.png" alt="Logotipo de tu tienda">
-        </section>
      <div class="eslogan">
         <h1>
             Viste con actitud, destaca con elegancia.
         </h1>
      </div>
-    </div>
-    <div class="prin">
-      <img src="./../assets/imagen1.png" alt="imagen principal">
     </div>
   </div>
 </template>
--- src/view/GeneralTemplate.vue
+++ src/view/GeneralTemplate.vue
@@ -6,17 +6,8 @@ import navbar from './../components/reutilisables/navbar.vue'
     <navbar />
     <div>
         <div class="imagen-fondo">
-            <img src="./../assets/fondo1.png" />
+          
         </div>
         <router-view />
     </div>
 </template>
-
-<style>
-.imagen-fondo{
-    position: fixed;
-    z-index: -1;
-    opacity: 0.3;
-}
-</style>
-
